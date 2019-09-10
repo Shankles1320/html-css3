3218cb90c4085c610ee4e1414b74319429fa74a2
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-09-10 08:55:37 -0700

    afternoon html 3

---
+++ src/Component/banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Banner = (props) => {
+	return (
+		<div classname="background-image">
+			<div className="banner-container">
+				<div classname="text">
+					<h2> Welcome to Our Studio</h2>
+					<h1>IT'S NICE TO MEET YOU</h1>
+					<button className="tell-more">TELL ME MORE</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
